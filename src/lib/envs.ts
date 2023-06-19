cf8b3fd546bf7df8c8f7f086691a7186d6a03a03
GonzaloAxelH <gonzaloaxelh@gmail.com>
2023-06-18 19:42:51 -0500

    adasd

--- src/lib/envs.ts
+++ src/lib/envs.ts
@@ -1,5 +1,4 @@
 export const isProduction = process.env.NODE_ENV === "production";
 export const isDevelopment = process.env.NODE_ENV === "development";
 
-export const siteURL =
-  (process.env.NEXT_PUBLIC_URL as string) || "http://localhost:8000";
+export const siteURL = process.env.NEXT_PUBLIC_URL_BACKEND;
